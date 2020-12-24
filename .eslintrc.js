52fd3b1369c1069b6f564726bba114b346b3d524
Hayoun Lee <sidhlee@gmail.com>
2020-12-24 10:45:17 -0500

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2020: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier', 'plugin:node/recommended'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'func-names': 'off',
+    'no-process-exit': 'off',
+    'object-shorthand': 'off',
+    'class-methods-use-this': 'off',
+    'prefer-template': 'off',
+    'prefer-destructuring': 'off',
+    'arrow-body-style': 'off',
+  },
+}
