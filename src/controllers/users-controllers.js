87bbad83c91535dcff70d8792671b40a2876a347
Hayoun Lee <sidhlee@gmail.com>
2020-12-24 20:09:06 -0500

    feat: add PATCH /users/me

--- src/controllers/users-controllers.js
+++ src/controllers/users-controllers.js
@@ -49,7 +49,25 @@ const login = async (req, res, next) => {
   })
 }
 
+const updateUser = async (req, res, next) => {
+  const updatingFields = Object.keys(req.body)
+  const allowedUpdates = ['name', 'email', 'password']
+  const isValid = updatingFields.every((field) =>
+    allowedUpdates.includes(field)
+  )
+  if (!isValid) {
+    return next(new HttpError('Invalid updates!', 400))
+  }
+  updatingFields.forEach((field) => {
+    req.user[field] = req.body[field]
+  })
+  await req.user.save()
+
+  return res.json(req.user)
+}
+
 module.exports = {
   signup,
   login,
+  updateUser,
 }
--- src/routers/users-router.js
+++ src/routers/users-router.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const { auth } = require('../middlewares/auth')
 
-const { signup, login } = require('../controllers/users-controllers')
+const {
+  signup,
+  login,
+  updateUser,
+} = require('../controllers/users-controllers')
 
 const usersRouter = express.Router()
 
 usersRouter.post('/signup', signup)
 usersRouter.post('/login', login)
 usersRouter.get('/me', auth, (req, res) => res.json({ user: req.user }))
+usersRouter.patch('/me', auth, updateUser)
 
 module.exports = { usersRouter }
