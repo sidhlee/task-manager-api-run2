4a137ea2c0c84fe5bb6cd3cde46076c96525fd0f
Hayoun Lee <sidhlee@gmail.com>
2020-12-24 19:39:04 -0500

    feat: add /me endpoint

--- src/routers/users-router.js
+++ src/routers/users-router.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { auth } = require('../middlewares/auth')
 
 const { signup, login } = require('../controllers/users-controllers')
 
@@ -6,5 +7,6 @@ const usersRouter = express.Router()
 
 usersRouter.post('/signup', signup)
 usersRouter.post('/login', login)
+usersRouter.get('/me', auth, (req, res) => res.json({ user: req.user }))
 
 module.exports = { usersRouter }
