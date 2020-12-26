728747a805f068135e0f1b20e2ab2ea75716fd67
Hayoun Lee <sidhlee@gmail.com>
2020-12-25 21:11:35 -0500

    test: add dummy user data and add user test names

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,9 +1,33 @@
+const mongoose = require('mongoose')
+
 const User = require('../../src/models/user')
+
+const userOneId = new mongoose.Types.ObjectId()
+const userOne = {
+  _id: userOneId,
+  name: 'one',
+  email: 'one@test.com',
+  password: '123123',
+}
+
+const userTwoId = new mongoose.Types.ObjectId()
+const userTwo = {
+  _id: userTwoId,
+  name: 'two',
+  email: 'two@test.com',
+  password: '123123',
+}
 
 const setupDatabase = async () => {
   await User.deleteMany()
+  await new User(userOne).save()
+  await new User(userTwo).save()
 }
 
 module.exports = {
+  userOneId,
+  userOne,
+  userTwoId,
+  userTwo,
   setupDatabase,
 }
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,8 +1,17 @@
 const request = require('supertest')
-const { setupDatabase } = require('./fixtures/db')
+const {
+  userOneId,
+  userOne,
+  userTwoId,
+  userTwo,
+  setupDatabase,
+} = require('./fixtures/db')
 const app = require('../src/app')
+const User = require('../src/models/user')
 
 beforeEach(setupDatabase)
+
+// POST: /signup
 
 test('Should signup a new user', async () => {
   const response = await request(app)
@@ -13,4 +22,77 @@ test('Should signup a new user', async () => {
       password: '123123',
     })
     .expect(201)
+
+  const user = await User.findOne({ email: 'test@test.com' })
+
+  expect(user).not.toBeNull()
+
+  expect(response.body).toMatchObject({
+    user: { name: 'test', email: 'test@test.com' },
+  })
+
+  expect(response.body.token.length).toBeGreaterThan(10)
 })
+
+test('Should not signup with invalid name')
+
+test('Should not signup with invalid email')
+
+test('Should not signup with invalid password')
+
+// POST: /login
+
+test('Should login existing user', async () => {
+  const response = await request(app)
+    .post('/users/login')
+    .send({
+      email: userOne.email,
+      password: userOne.password,
+    })
+    .expect(200)
+
+  expect(response.body.user).toMatchObject({
+    _id: userOneId.toHexString(),
+    name: userOne.name,
+    email: userOne.email,
+  })
+
+  expect(response.body.token.length).toBeGreaterThan(10)
+})
+
+test('Should not login with wrong password', async () => {
+  await request(app)
+    .post('/users/login')
+    .send({
+      email: userOne.email,
+      password: 'wrongpassword!',
+    })
+    .expect(422)
+})
+
+// GET: /me
+
+test('Should get profile for authenticated user')
+
+test('Should not get profile for unauthenticated user')
+
+// PATCH: /me
+test('Should update valid user fields')
+
+test('Should not update invalid user fields')
+
+test('Should not update user if unauthenticated')
+
+test('Should not update user with invalid name')
+
+test('Should not update user with invalid email')
+
+test('SHould not update user with invalid password')
+
+// DELETE: /me
+test('Should delete user')
+
+test('Should not delete unauthenticated user')
+
+// POST: /me/avatar
+test('Should upload avatar image')
